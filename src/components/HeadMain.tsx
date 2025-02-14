fb106754267694eb58492e813c55496bb81894eb
anjumolgeorge <georgeanjumol782@gmail.com>
2025-02-14 21:39:33 +0530

    edit

--- src/components/HeadMain.tsx
+++ src/components/HeadMain.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaSearch } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
 
 const HeadMain = () => {
   return (
@@ -8,10 +10,20 @@ const HeadMain = () => {
             Logo
         </div>
 
-        <div className=' sm:w-[200px] md:w-[30%] relative'>
-            <input className='border-gray-200 border p-2 px-6 rounded-lg w-full' 
+        <div className=' w-full sm:w-[300px] md:w-[70%] relative'>
+            <input className='border-gray-200 border p-2 px-4 rounded-lg w-full' 
             type='text'
-            placeholder='Enter any product name...'/>
+            placeholder='Enter any product name...'
+            />
+            <FaSearch className=' absolute right-0 top-0 mr-3 mt-3 text-gray-400' size={20} />
+        </div>
+        <div className=' hidden lg:flex gap-4 text-gray-500 text-[30px]'>
+            <div className=' relative'>
+            <FaRegHeart />
+            <div className=' bg-red-600 rounded-full absolute top-0 right-0 w-[18px] h-[18px] text[12px] text-white grid place-items-center translate-x-1 -translate-y-1'>
+                0
+            </div>
+            </div>
         </div>
       </div>
     </div>
