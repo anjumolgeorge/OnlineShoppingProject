2ce17e6558834a67779f17f9e4aeac23eba4e945
anjumolgeorge <georgeanjumol782@gmail.com>
2025-03-01 10:56:38 +0530

    edit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import Navbar from "@/components/Navbar";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"
 import Footer from "@/components/Footer";
+import MobNavbar from "@/components/MobNavbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           <HeaderTop/>
           <HeadMain/>
           <Navbar/>
+          <MobNavbar/>
         {children}
         <Footer/>
       </body>
